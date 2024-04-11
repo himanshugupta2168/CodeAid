0b289c70842f5d126f854e8f5b234c5149691fd1
himanshu2168.be21@chitkara.edu.in <himanshu2168.be21@chitkara.edu.in>
2024-04-12 02:51:57 +0530

    linked routes to controler functions

--- backEnd/src/Routes/userRoutes.ts
+++ backEnd/src/Routes/userRoutes.ts
@@ -2,8 +2,8 @@ import express from "express"
 import { PrismaClient } from "@prisma/client";
 const prisma= new PrismaClient()
 const router = express.Router();
-import { signUpController, signInController} from "../Controllers/userControllers";
+import { signUpController, signInController, fogetPasswordLinkSender} from "../Controllers/userControllers";
 router.post("/signup",signUpController)
 router.post("/signin", signInController)
-router.patch("/forgetPassword", ()=>{})
+router.patch("/forgetPassword", fogetPasswordLinkSender)
 export default router
